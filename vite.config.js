5651802d294f7b09f4ddf4db95e54ddfef0bdaf8
William Corrêa Mendes <william.mendes@ifma.edu.br>
2025-08-27 19:32:07 -0300

    Ajuste vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/ed_modulo4/', // necessário para GitHub Pages em project site
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
