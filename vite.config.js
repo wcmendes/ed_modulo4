6d2ff23786bb6ffc7fb836254ab29a9470cca6f7
William Mendes <wcmendes@gmail.com>
2025-08-28 14:57:13 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/ed_modulo4/', // necessário para GitHub Pages em project site
+  base: './', // necessário para GitHub Pages em project site
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
